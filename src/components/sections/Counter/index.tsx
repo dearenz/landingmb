9f981b454eeac16a00a98e5f60bba0589f43380b
dearenz <dearenteo@gmail.com>
2024-04-17 12:50:51 +0700

    fix responsive

--- src/components/sections/Counter/index.tsx
+++ src/components/sections/Counter/index.tsx
@@ -6,13 +6,13 @@ const Counter: FC<CounterProps> = ({}) => {
   return (
     <section className="bg-[#e7f0fd] py-16">
       <div className="container mx-auto flex flex-col gap-10 items-center">
-        <div className="flex gap-4 text-center space-x-40">
+        <div className="flex gap-4 text-center space-x-20 md:space-x-40">
           <div className="flex flex-col gap-3 text-center">
-            <h1 className="font-bold text-primary text-7xl">249</h1>
+            <h1 className="font-bold text-primary text-5xl md:text-7xl">249</h1>
             <p className="text-secondary text-2xl">Partnership</p>
           </div>
           <div className="flex flex-col gap-3 text-center">
-            <h1 className="font-bold text-primary text-7xl">184</h1>
+            <h1 className="font-bold text-primary text-5xl md:text-7xl">184</h1>
             <p className="text-secondary text-2xl">Projects</p>
           </div>
         </div>
