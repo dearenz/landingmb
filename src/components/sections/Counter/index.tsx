114d15921d34441ac7856a5b855dfe932d97e244
dearenz <dearenteo@gmail.com>
2024-04-17 13:12:06 +0700

    add animate counter

--- src/components/sections/Counter/index.tsx
+++ src/components/sections/Counter/index.tsx
@@ -8,11 +8,17 @@ const Counter: FC<CounterProps> = ({}) => {
       <div className="container mx-auto flex flex-col gap-10 items-center">
         <div className="flex gap-4 text-center space-x-20 md:space-x-40">
           <div className="flex flex-col gap-3 text-center">
-            <h1 className="font-bold text-primary text-5xl md:text-7xl">249</h1>
+            <span className="font-bold text-primary text-5xl md:text-7xl animate-[counter_3s_ease-out_forwards] tabular-nums [counter-set:_num_var(--num-partnership)] before:content-[counter(num)]">
+              {" "}
+              <h1 className="sr-only">249</h1>
+            </span>
             <p className="text-secondary text-2xl">Partnership</p>
           </div>
           <div className="flex flex-col gap-3 text-center">
-            <h1 className="font-bold text-primary text-5xl md:text-7xl">184</h1>
+            <span className="font-bold text-primary text-5xl md:text-7xl animate-[counter_3s_ease-out_forwards] tabular-nums [counter-set:_num_var(--num-projects)] before:content-[counter(num)]">
+              {" "}
+              <h1 className="sr-only">184</h1>
+            </span>
             <p className="text-secondary text-2xl">Projects</p>
           </div>
         </div>
